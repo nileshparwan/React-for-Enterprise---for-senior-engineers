e733a7c3f18dfbd074318d157e80919bfb33c869
KoshalParwan <koshal.parwan@fasst.io>
2024-10-08 12:04:41 +0400

    feat: add commitizen

---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,9 @@
+ import React from 'react';
+ import ReactDOM from 'react-dom';
+
+ import { Button } from '@ds.e/react';
+
+ ReactDOM.render(
+   <Button label='Hello World' />,
+   document.getElementById('root')
+ );
